e0dd5162079302fe876291c2ed9906b17a26e4b3
liren <liren@9zhitx.com>
2017-01-25 10:07:30 +0800

    base

---
+++ liren-spring-data/spring-data-jpa/src/main/java/org/spring/data/jpa/App.java
@@ -0,0 +1,13 @@
+package org.spring.data.jpa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ liren-spring-data/spring-data-mybatis/src/main/java/org/spring/data/mybatis/App.java
@@ -0,0 +1,13 @@
+package org.spring.data.mybatis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
