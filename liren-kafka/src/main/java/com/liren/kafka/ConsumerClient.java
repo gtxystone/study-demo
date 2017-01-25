c9ec3122d69bad8efa86f3fb96661d720ca8d1e0
liren <liren@9zhitx.com>
2017-01-25 09:58:39 +0800

    base

---
+++ liren-kafka/src/main/java/com/liren/kafka/ConsumerClient.java
@@ -0,0 +1,32 @@
+package com.liren.kafka;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+public class ConsumerClient {
+
+	public static void main(String[] args) {
+		Properties props = new Properties();
+//		props.put("bootstrap.servers", "localhost:9092");
+		props.put("bootstrap.servers", "192.168.153.136:9092,192.168.153.137:9092,192.168.153.138:9092");
+//		props.put("group.id", "test");
+		props.put("group.id", "console-consumer-62593");
+		props.put("enable.auto.commit", "true"); // 自动commit
+		props.put("auto.commit.interval.ms", "1000"); // 定时commit的周期
+		props.put("session.timeout.ms", "30000"); // consumer活性超时时间
+		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+//		consumer.subscribe(Arrays.asList("foo", "bar")); // subscribe，foo，bar，两个topic
+		consumer.subscribe(Arrays.asList("mytopic")); // subscribe，foo，bar，两个topic
+		while (true) {
+			ConsumerRecords<String, String> records = consumer.poll(100); // poll
+			for (ConsumerRecord<String, String> record : records)
+				System.out.printf("offset = %d, key = %s, value = %s", record.offset(), record.key(), record.value());
+		}
+	}
+}
---
+++ liren-kafka/src/main/java/com/liren/kafka/ProducerClient.java
@@ -0,0 +1,36 @@
+package com.liren.kafka;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+/**
+ * Hello world!
+ *
+ */
+public class ProducerClient {
+
+	public static void main(String[] args) {
+		Properties props = new Properties();
+//		props.put("bootstrap.servers", "localhost:9092");
+		props.put("bootstrap.servers", "192.168.153.136:9092,192.168.153.137:9092,192.168.153.138:9092");
+		props.put("acks", "all"); // ack方式，all，会等所有的commit最慢的方式
+		props.put("retries", 0); // 失败是否重试，设置会有可能产生重复数据
+		props.put("batch.size", 16384); // 对于每个partition的batch buffer大小
+		props.put("linger.ms", 1); // 等多久，如果buffer没满，比如设为1，即消息发送会多1ms的延迟，如果buffer没满
+		props.put("buffer.memory", 33554432); // 整个producer可以用于buffer的内存大小
+		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+		Producer<String, String> producer = new KafkaProducer<>(props);
+		
+		
+		for (int i = 0; i < 100; i++)
+			producer.send(new ProducerRecord<String, String>("mytopic", Integer.toString(i), Integer.toString(i)));
+
+		producer.close();
+	}
+
+}
