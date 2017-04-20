427ba76c50fd41d41a940d038ce455e43490931f
liren <liren@9zhitx.com>
2017-04-20 18:34:06 +0800

    commons

---
+++ liren-commons/src/main/java/com/liren/liren_commons/App.java
@@ -0,0 +1,13 @@
+package com.liren.liren_commons;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
